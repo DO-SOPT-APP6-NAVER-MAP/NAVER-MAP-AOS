8efaa44e0acb95235aba0e780081fcb8673c450a
Sangwook123 <operawook@catholic.ac.kr>
2023-11-22 03:05:55 +0900

    [feat] 스티키 헤더 구현

--- app/src/main/java/org/sopt/navermap/presentation/detail/DetailActivity.kt
+++ app/src/main/java/org/sopt/navermap/presentation/detail/DetailActivity.kt
@@ -1,12 +1,33 @@
 package org.sopt.navermap.presentation.detail
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import org.sopt.navermap.R
+import org.sopt.navermap.databinding.ActivityDetailBinding
+import org.sopt.navermap.util.binding.ViewBindingActivity
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity :
+    ViewBindingActivity<ActivityDetailBinding>({ ActivityDetailBinding.inflate(it) }) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        setContentView(binding.root)
+
+        var intColorCode = 255
+
+        binding.detailAppbar.addOnOffsetChangedListener{ _, verticalOffset ->
+            intColorCode = -verticalOffset/2
+            if (intColorCode > 255) {
+                intColorCode = 255
+            }
+
+            if(intColorCode > 50) {
+                binding.ivDetailBack.setColorFilter(getColor(R.color.gray_4))
+                binding.ivDetailClose.setColorFilter(getColor(R.color.gray_4))
+            } else {
+                binding.ivDetailBack.setColorFilter(getColor(R.color.white))
+                binding.ivDetailClose.setColorFilter(getColor(R.color.white))
+            }
+            binding.tbDetailTitle.background.alpha = intColorCode
+            binding.tvDetailMain.alpha = intColorCode.toFloat()/255f
+        }
     }
 }
