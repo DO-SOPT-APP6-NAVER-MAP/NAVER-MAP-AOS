5de608677b398eddaef4b6a2f7933c8d2b982f30
memoryBangwool <jihyuneee01@gmail.com>
2023-11-22 21:28:07 +0900

    [add] MainsearchGridViewHolder 뷰홀더 추가

---
+++ app/src/main/java/org/sopt/navermap/presentation/main/MainsearchGridViewHolder.kt
@@ -0,0 +1,9 @@
+package org.sopt.navermap.presentation.main
+
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.navermap.databinding.ItemResultGridBinding
+
+class MainsearchGridViewHolder (private val binding: ItemResultGridBinding):
+        RecyclerView.ViewHolder(binding.root) {
+
+        }
