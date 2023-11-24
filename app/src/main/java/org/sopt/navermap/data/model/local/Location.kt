6a72e8e76a95c69a4858bbc6ddf446da4398553a
memoryBangwool <jihyuneee01@gmail.com>
2023-11-25 02:47:43 +0900

    [feat] Location data class추가

---
+++ app/src/main/java/org/sopt/navermap/data/model/local/Location.kt
@@ -0,0 +1,10 @@
+package org.sopt.navermap.data.model.local
+
+data class Location(
+    val id: Int,
+    val name: String,
+    val address: String,
+    val distance: String,
+    val category: String,
+    val review: Int
+)
--- app/src/main/java/org/sopt/navermap/presentation/main/MainActivityViewModel.kt
+++ app/src/main/java/org/sopt/navermap/presentation/main/MainActivityViewModel.kt
@@ -1,4 +1,22 @@
 package org.sopt.navermap.presentation.main
 
 class MainActivityViewModel {
+    val mockLocationList = {
+        {
+            "id" : 1,
+            "name": "알고리즘",
+            "address" :"서울 강남구 도산대로49길 8 2층",
+            "distance" : "31km",
+            "category":"퓨전음식",
+            "review" : 12
+        },
+        {
+            "id" : 2,
+            "name": "알고",
+            "adress" : "서울 강동구 천호대로151길 19 2층",
+            "distance" : "4.4km",
+            "category" : "장난감",
+            "review" : 466
+        }
+    ]}
 }
