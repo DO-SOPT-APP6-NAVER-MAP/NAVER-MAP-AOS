5491e27098ac51a6cf073937ccdaf3f76b3c7592
KIM MINJEONG <minjeong030420@gmail.com>
2023-11-30 17:29:43 +0900

    [ui] ui 수정

--- app/src/main/java/org/sopt/navermap/presentation/search/result/TestSearchActivity.kt
+++ app/src/main/java/org/sopt/navermap/presentation/search/result/TestSearchActivity.kt
@@ -12,6 +12,7 @@ import org.sopt.dosopttemplate.util.binding.DataBindingActivity
 import org.sopt.navermap.R
 import org.sopt.navermap.data.model.local.SearchResult
 import org.sopt.navermap.databinding.ActivityTestSearchBinding
+import org.sopt.navermap.presentation.detail.DetailActivity
 
 class TestSearchActivity :
     DataBindingActivity<ActivityTestSearchBinding>(R.layout.activity_test_search){
@@ -56,7 +57,7 @@ class TestSearchActivity :
 
                     BottomSheetBehavior.STATE_EXPANDED -> {
                         Log.d("testActivity: ", "bottomSheet 펼치기 ")
-                        val intent = Intent(this@TestSearchActivity, TestSearch2Activity::class.java)
+                        val intent = Intent(this@TestSearchActivity, DetailActivity::class.java)
                         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
                         startActivity(intent)
                     }
