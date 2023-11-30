275378e48525e4e4134cc9e9a7546396c3b77882
CHANGEL <107169027+CHANGEL1004@users.noreply.github.com>
2023-11-30 21:42:16 +0900

    [fix] : 생성자 오류 수정

--- app/src/main/java/org/sopt/navermap/presentation/route/RouteViewModelFactory.kt
+++ app/src/main/java/org/sopt/navermap/presentation/route/RouteViewModelFactory.kt
@@ -1,11 +1,12 @@
 package org.sopt.navermap.presentation.route
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import org.sopt.navermap.data.ServicePool
 import org.sopt.navermap.data.repository.RouteRepository
 
-class RouteViewModelFactory : ViewModelProvider.Factory {
+class RouteViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(RouteViewModel::class.java)) {
             val repository =
