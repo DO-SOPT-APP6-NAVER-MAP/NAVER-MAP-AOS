312fadf365d3404608453cc460aae546e62d2490
Sangwook123 <operawook@catholic.ac.kr>
2023-12-01 19:38:38 +0900

    [feat] detail뷰 클릭이벤트

--- app/src/main/java/org/sopt/navermap/presentation/detail/DetailActivity.kt
+++ app/src/main/java/org/sopt/navermap/presentation/detail/DetailActivity.kt
@@ -1,10 +1,12 @@
 package org.sopt.navermap.presentation.detail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import org.sopt.dosopttemplate.util.binding.DataBindingActivity
 import org.sopt.navermap.R
 import org.sopt.navermap.databinding.ActivityDetailBinding
+import org.sopt.navermap.presentation.route.RouteActivity
 
 class DetailActivity :
     DataBindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
@@ -15,7 +17,10 @@ class DetailActivity :
         setContentView(binding.root)
 
         viewModel.getDetail(1)
-
+        binding.ivDetailArrive.setOnClickListener {
+            val intent = Intent(this, RouteActivity::class.java)
+            startActivity(intent)
+        }
         initGetDetailResultObserver()
         initAppbarOffsetChangedListener()
     }
