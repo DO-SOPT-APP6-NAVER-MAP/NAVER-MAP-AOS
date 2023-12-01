564ad0dfba4c0d2e7cb5eaf1f1a7d6c3c8f18b64
Sangwook123 <operawook@catholic.ac.kr>
2023-12-01 19:38:56 +0900

    [feat] 어댑터 클릭이벤트

--- app/src/main/java/org/sopt/navermap/presentation/main/LocationListAdapter.kt
+++ app/src/main/java/org/sopt/navermap/presentation/main/LocationListAdapter.kt
@@ -7,14 +7,18 @@ import androidx.recyclerview.widget.RecyclerView
 import org.sopt.navermap.data.model.remote.response.ResponseSearchNameDto
 import org.sopt.navermap.databinding.ItemResultListBinding
 
-class LocationListAdapter(private val context: Context, private val enteredName: String) :
+class LocationListAdapter(
+    private val context: Context,
+    private val enteredName: String,
+    private val onClicked: (ResponseSearchNameDto.ResponseData) -> Unit
+) :
     RecyclerView.Adapter<LocationListViewHolder>() {
     private val inflater by lazy { LayoutInflater.from(context) }
     private var locationList: List<ResponseSearchNameDto.ResponseData> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationListViewHolder {
         val binding = ItemResultListBinding.inflate(inflater, parent, false)
-        return LocationListViewHolder(binding, context, enteredName)
+        return LocationListViewHolder(binding, context, onClicked, enteredName)
     }
 
     override fun onBindViewHolder(holder: LocationListViewHolder, position: Int) {
